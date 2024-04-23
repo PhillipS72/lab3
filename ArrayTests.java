04ac38647fd9eff994017bc34601996b29396883
Phillip Sage Schiffman <146408085+PhillipS72@users.noreply.github.com>
2024-04-23 13:37:24 -0700

    Fixing some bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
-    int[] input1 = { };
+    int[] input1 = { 3 };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {9,8,7,6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6,7,8,9}, input1);
+	}
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {9,8,7,6};
+    assertArrayEquals(new int[]{6,7,8,9}, ArrayExamples.reversed(input1));
   }
 }
